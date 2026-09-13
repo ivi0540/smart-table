2d9748f3c7e6f8f17f1e7d961bccf8f9de815b0f
ivi0540 <ivi0540@gmail.com>
2026-09-13 19:50:51 +0300

    Project Work 7, Step 5. Server-side sorting

--- src/main.js
+++ src/main.js
@@ -51,6 +51,7 @@ async function render(action) {
 
   query = applySearching(query, state, action); // result заменяем на query
   query = applyFiltering(query, state, action);
+  query = applySorting(query, state, action); // result заменяем на query
   query = applyPagination(query, state, action); // обновляем query
   const { total, items } = await api.getRecords(query);
   updatePagination(total, query); // перерисовываем пагинатор
