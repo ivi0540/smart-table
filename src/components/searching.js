ff916f583e82d3b87afe2d734152ae64e173886e
ivi0540 <ivi0540@gmail.com>
2026-09-13 19:19:27 +0300

    Project Work 7, Step 4. Server-side search

--- src/components/searching.js
+++ src/components/searching.js
@@ -1,4 +1,4 @@
-import { createComparison } from "../lib/compare.js";
+// import { createComparison } from "../lib/compare.js";
 
 export function initSearching(searchField) {
   // @todo: #5.1 — настроить компаратор
@@ -15,19 +15,13 @@ export function initSearching(searchField) {
   //     }
   // ]);
 
-  return (data, state, action) => {
-    // @todo: #5.2 — применить компаратор
-
-    const searchText = state[searchField]?.toLowerCase().trim() || "";
-    if (!searchText) return data;
-
-    const fields = ["date", "customer", "seller"];
-
-    return data.filter((row) => {
-      return fields.some((field) => {
-        const value = row[field]?.toString().toLowerCase() || "";
-        return value.includes(searchText);
-      });
-    });
+  return (query, state, action) => {
+    // result заменили на query
+    return state[searchField]
+      ? Object.assign({}, query, {
+          // проверяем, что в поле поиска было что-то введено
+          search: state[searchField], // устанавливаем в query параметр
+        })
+      : query; // если поле с поиском пустое, просто возвращаем query без изменений
   };
 }
--- src/main.js
+++ src/main.js
@@ -49,6 +49,7 @@ async function render(action) {
   // result = applySorting(result, state, action);
   // result = applyPagination(result, state, action);
 
+  query = applySearching(query, state, action); // result заменяем на query
   query = applyFiltering(query, state, action);
   query = applyPagination(query, state, action); // обновляем query
   const { total, items } = await api.getRecords(query);
