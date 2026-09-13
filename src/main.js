ee7a2aedfaf83ca4b732feab53f6e459f87c3d3f
ivi0540 <ivi0540@gmail.com>
2026-09-13 14:07:12 +0300

    Project Work 7, Step 0: Preparation and Planning

--- src/main.js
+++ src/main.js
@@ -42,10 +42,10 @@ function render(action) {
   let result = [...data]; // копируем для последующего изменения
   // @todo: использование
 
-  result = applySearching(result, state, action);
-  result = applyFiltering(result, state, action);
-  result = applySorting(result, state, action);
-  result = applyPagination(result, state, action);
+  // result = applySearching(result, state, action);
+  // result = applyFiltering(result, state, action);
+  // result = applySorting(result, state, action);
+  // result = applyPagination(result, state, action);
 
   sampleTable.render(result);
 }
@@ -80,10 +80,10 @@ const applySorting = initSorting([
   sampleTable.header.elements.sortByTotal,
 ]);
 
-const applyFiltering = initFiltering(sampleTable.filter.elements, {
-  // передаём элементы фильтра
-  searchBySeller: indexes.sellers, // для элемента с именем searchBySeller устанавливаем массив продавцов
-});
+// const applyFiltering = initFiltering(sampleTable.filter.elements, {
+//   // передаём элементы фильтра
+//   searchBySeller: indexes.sellers, // для элемента с именем searchBySeller устанавливаем массив продавцов
+// });
 
 const applySearching = initSearching("search");
 
